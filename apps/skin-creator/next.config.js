066d3702addb5e8266ae40863b41e4da390863be
FreeRiverHouse <mattiapetrucciani@mac.com>
2026-01-30 14:48:35 -0800

    fix: skin-creator basePath for static deployment

--- apps/skin-creator/next.config.js
+++ apps/skin-creator/next.config.js
@@ -2,5 +2,7 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/static-games/skin-creator',
+  assetPrefix: '/static-games/skin-creator',
 };
 module.exports = nextConfig;
